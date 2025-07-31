865152468098188f8acbf131f0778b1e3b4e031f
mehmet ertugrul evyapar <mertugrulevyapar@gmail.com>
2025-07-31 18:22:14 +0300

    lumahome

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./Luma_Home.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <img
+        src={logo}
+        alt="LumaHome Logo"
+        style={{ height: "150px", width: "150px" }}
+      />
+      <p>Sitemiz Yapım Aşamasındadır...</p>
+    </div>
+  );
+}
+
+export default App;
